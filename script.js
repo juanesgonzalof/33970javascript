2f123d2a6b6b71f9544b9840e0b7e3c0bd3c5015
Gonzalo Juanes <rage.against.themachine.740@gmail.com>
2022-07-18 20:59:48 -0300

    Primer desafio complementario

---
+++ script.js
@@ -0,0 +1,9 @@
+let usuario = 'Coderhouse' .toLowerCase
+let pass = '33970'
+
+do{
+    usuario = prompt('Ingrese su usuario')
+    pass = prompt('Ingrese su contraseña')
+} while(usuario != 'coderhouse')
+       (pass != '33970')
+        alert('Sesion iniciada exitosamente')
